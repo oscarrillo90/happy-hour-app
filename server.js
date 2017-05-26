46907207bba2bf959d35171375987adf6018c699
oscar carrillo <oscarrillo008@outlook.com>
2017-05-26 16:48:20 -0500

    added process env listeners

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-  mongoose.connect('mongodb://localhost/todo-app');
+  mongoose.connect(process.env.MONDOB_URI || 'mongodb://localhost/todo-app');
 // MIDDLEWARE
 app.use(express.static('public'));
 
@@ -13,6 +13,6 @@ app.get('/', function(req, res){
   res.json({message: "IT WORKS!"});
 })
 // SERVER START
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("HTTP server listening at localhost:3000");
 });
