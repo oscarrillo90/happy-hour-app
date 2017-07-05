aae263e3516751cb8b9d3a20e37ff95f062a203e
oscar carrillo <oscarrillo008@outlook.com>
2017-07-05 11:00:15 -0500

    Update yelp.js

--- config/yelp.js
+++ config/yelp.js
@@ -5,9 +5,8 @@ var yelp = require('yelp-fusion');
 
 // Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
-var clientId = 'V2QqY10c5zFZE84_xaQwYQ';
-var clientSecret = 'QjWl5JQMnwhIYhFwlrpvbOZ99HsaJTYbufGKLt5CBRO9KxVTfcaZFmXb4xxQEepL';
-
+var clientId = 
+var clientSecret = 
 var searchRequest = {
   term:'happy hour',
   location: 'austin, tx',
